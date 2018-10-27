2b641d79f3f3ede568b6b1768e5f231ac6378785
David Sevilla Martín <dsevilla192@icloud.com>
2018-10-26 20:23:36 -0400

    Use @fof/components for admin JS

--- js/src/admin/index.js
+++ js/src/admin/index.js
@@ -1,5 +1,17 @@
-import SecureHttpsSettingsModal from './components/SecureHttpsSettingsModal';
+import SettingsModal from '@fof/components/admin/settings/SettingsModal';
+import BooleanItem from '@fof/components/admin/settings/items/BooleanItem';
 
 app.initializers.add('fof/secure-https', () => {
-    app.extensionSettings['fof-secure-https'] = () => app.modal.show(new SecureHttpsSettingsModal());
+    app.extensionSettings['fof-secure-https'] = () =>
+        app.modal.show(
+            new SettingsModal({
+                title: 'FriendsOfFlarum Secure HTTPS',
+                className: 'FoFSecureHttpsSettingsModal',
+                items: [
+                    <BooleanItem key="fof-secure-https.proxy" cast={Number}>
+                        {app.translator.trans('fof-secure-https.admin.settings.replace')}
+                    </BooleanItem>,
+                ],
+            })
+        );
 });
